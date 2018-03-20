fca036d4a82fa18d08768cdb5d89428d992cf0a9
lorence <lorence@enclave.vn>
2018-03-20 11:00:27 +0700

    Stop

---
+++ movie/bundle/src/main/java/com/moviecomponent/moviecomponent/core/service/MovieService.java
@@ -0,0 +1,11 @@
+package com.moviecomponent.moviecomponent.core.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.adobe.cq.sightly.WCMUse;
+
+public class MovieService extends WCMUse{
+}
