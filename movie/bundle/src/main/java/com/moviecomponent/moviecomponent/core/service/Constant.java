071a5189ace9a24a476a09a572c3c2d8395297c3
lorence <lorence@enclave.vn>
2018-03-20 11:13:36 +0700

    Remove folder movie

--- movie/bundle/src/main/java/com/moviecomponent/moviecomponent/core/service/Constant.java
+++
@@ -1,8 +0,0 @@
-package com.moviecomponent.moviecomponent.core.service;
-
-public class Constant {
-
-    public final static String SEARCH_PATH = "/content/dam/moviecomponent/Upcoming/";
-    public final static String SEARCH_TYPE = "dam:Asset";
-    
-}
