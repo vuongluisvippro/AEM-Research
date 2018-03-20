d01172303de3ff519d2429201b868d134f489e3d
lorence <lorence@enclave.vn>
2018-03-20 10:45:22 +0700

    Add more files

---
+++ movie/bundle/src/test/java/com/moviecomponent/moviecomponent/core/service/SimpleUnitTest.java
@@ -0,0 +1,14 @@
+package com.moviecomponent.moviecomponent.core.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SimpleUnitTest {
+
+    @Test
+    public void someTest() {
+        assertTrue(true);
+    }
+
+}
