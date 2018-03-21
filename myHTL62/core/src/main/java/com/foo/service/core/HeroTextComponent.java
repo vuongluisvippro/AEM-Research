9c727b1c1ac49ea00ca361959df08e1feaf82226
lorence <lorence@enclave.vn>
2018-03-21 10:03:00 +0700

    Build successfully

---
+++ myHTL62/core/src/main/java/com/foo/service/core/HeroTextComponent.java
@@ -0,0 +1,34 @@
+package com.foo.service.core;
+
+import com.adobe.cq.sightly.WCMUsePojo;
+
+import javax.jcr.Node;
+
+public class HeroTextComponent extends WCMUsePojo
+{
+
+    /** The hero text bean. */
+    private HeroTextBean heroTextBean = null;
+
+    @Override
+    public void activate() throws Exception {
+
+
+
+        Node currentNode = getResource().adaptTo(Node.class);
+
+        heroTextBean = new HeroTextBean();
+
+        if(currentNode.hasProperty("jcr:Heading")){
+            heroTextBean.setHeadingText(currentNode.getProperty("./jcr:Heading").getString());
+        }
+        if(currentNode.hasProperty("jcr:description")){
+            heroTextBean.setDescription(currentNode.getProperty("./jcr:description").getString());
+        }
+
+    }
+
+    public HeroTextBean getHeroTextBean() {
+        return this.heroTextBean;
+    }
+}
